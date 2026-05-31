056561616026b0322fbe57885455e19db3a49fc8
Nematovm <nematovm764@gmail.com>
2026-05-31 14:35:25 +0500

    Fix Bugs

--- app/main.py
+++ app/main.py
@@ -27,19 +27,8 @@ app = FastAPI(title="ClearPath API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "https://multx.uz",
-        "https://www.multx.uz",
-        "https://app.multx.uz",      # ← QO'SHILDI
-        "https://api.multx.uz",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
